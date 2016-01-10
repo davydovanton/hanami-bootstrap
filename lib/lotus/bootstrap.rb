5f8e8fa30ed2158c3e4e1105667cba67b3defb46
Anton Davydov <antondavydov.o@gmail.com>
2016-01-10 21:46:43 +0300

    Load js and css files to lotus-assets

--- lib/lotus/bootstrap.rb
+++ lib/lotus/bootstrap.rb
@@ -2,6 +2,20 @@ require "lotus/bootstrap/version"
 
 module Lotus
   module Bootstrap
-    # Your code goes here...
+    class << self
+      def load!
+        ::Lotus::Assets.sources << assets_path
+      end
+
+      def gem_path
+        @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+      end
+
+      def assets_path
+        @assets_path ||= File.join gem_path, 'vendor', 'assets'
+      end
+    end
   end
 end
+
+Lotus::Bootstrap.load!
